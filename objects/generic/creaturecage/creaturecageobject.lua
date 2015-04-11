bcc13c8992a79336f26be848fca02155ab624342
pixlar <pixlar@kelseys-imac.home>
2015-04-11 00:40:49 -0400

    cloning cage

--- objects/generic/creaturecage/creaturecageobject.lua
+++ objects/generic/creaturecage/creaturecageobject.lua
@@ -10,23 +10,25 @@ function prep()
 		storage.knownPeers = {}
 		self.pause = false
 		self.hasPrepped = true
+		world.logInfo("Hello, dear friends?")
 		for i,item in ipairs(storage.heldItems) do
 			if item ~= nil then
-				print("attributes")
+				creature = {}
 				for index, value in pairs(item.parameters.projectileConfig.actionOnReap[1]) do
-					print(index)
+					creature[index] = value
 				end
-				print(item.name)
-				print(item.parameters.projectileConfig.actionOnReap)
-				print(item.parameters.projectileConfig.actionOnReap[1].type)
 			end
+			--world.spawnMonster(creature["type"], {entity.position()[1], entity.position()[2] + 1}, creature["arguments"])
 		end
 	end
 end
 function update(dt)
 
 	
-	if ~ self.hasPrepped then
+	if not self.hasPrepped then
 		prep()
+		--params = 
+		--spawnMonster("smallbiped", {entity.position()[1], entity.position()[2] + 1}, params)
+                                    
 	end	
 end
